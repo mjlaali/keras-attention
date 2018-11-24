a72303266eeffcb36a2b43f6926893e81c5fc400
Majid Laali <mjlaali@gmail.com>
2018-11-24 18:57:38 -0500

    Fix saving attention layer

--- src/mjlaali/keras/attention.py
+++ src/mjlaali/keras/attention.py
@@ -5,20 +5,21 @@ from tensorflow.python.keras import backend as K
 class AttentionCell(keras.layers.Layer):
 
     def __init__(self, rnn_cell, **kwargs):
-        super().__init__(**kwargs)
-        self.rnn_cell = rnn_cell
-        self.query_transformation = None
-        self.attention_logits_dense = None
-        self.internal_layers = None
+        super(AttentionCell, self).__init__(**kwargs)
+        self._cell = rnn_cell
+        self._query_transformation = None
+        self._attention_logits_dense = None
+        self._internal_layers = None
 
     def build(self, input_shape):
+        super(AttentionCell, self).build(input_shape)
         cell_input_shape, constant_input_shape = input_shape
-        self.rnn_cell.build(cell_input_shape)
+        self._cell.build(cell_input_shape)
         keys_dim = constant_input_shape[-1]
 
-        self.query_transformation = keras.layers.Dense(units=keys_dim, input_shape=(self.rnn_cell.state_size,))
-        self.attention_logits_dense = keras.layers.Dense(units=1, input_shape=(keys_dim,))
-        self.internal_layers = [self.rnn_cell, self.query_transformation, self.attention_logits_dense]
+        self._query_transformation = keras.layers.Dense(units=keys_dim, input_shape=(self._cell.state_size,))
+        self._attention_logits_dense = keras.layers.Dense(units=1, input_shape=(keys_dim,))
+        self._internal_layers = [self._cell, self._query_transformation, self._attention_logits_dense]
 
     def call(self, inputs, states, constants):
         if not isinstance(constants, (list, tuple)):
@@ -35,33 +36,45 @@ class AttentionCell(keras.layers.Layer):
         else:
             query = states[0]
 
-        query = self.query_transformation(query)
+        query = self._query_transformation(query)
         repeated_query = K.repeat(query, K.shape(keys)[1])
 
-        logits = self.attention_logits_dense(K.tanh(repeated_query + keys))
+        logits = self._attention_logits_dense(K.tanh(repeated_query + keys))
         attention_weights = keras.activations.softmax(logits, axis=1)
         attention_context = K.sum(attention_weights * values, axis=1, keepdims=False)
         inputs = inputs + attention_context
-        return self.rnn_cell.call(inputs, states)
+        return self._cell.call(inputs, states)
 
     def get_initial_state(self, inputs=None, batch_size=None, dtype=None):
-        return self.rnn_cell.get_initial_state(inputs, batch_size, dtype)
+        return self._cell.get_initial_state(inputs, batch_size, dtype)
 
     @property
     def state_size(self):
-        return self.rnn_cell.state_size
+        return self._cell.state_size
 
     @property
     def trainable_weights(self):
-        weights = super(AttentionCell, self).trainable_weights
-        for l in self.internal_layers:
+        weights = list(super(AttentionCell, self).trainable_weights)
+        for l in self._internal_layers:
             weights += l.trainable_weights
         return weights
 
     @property
     def non_trainable_weights(self):
-        weights = super(AttentionCell, self).non_trainable_weights
-        for l in self.internal_layers:
+        weights = list(super(AttentionCell, self).non_trainable_weights)
+        for l in self._internal_layers:
             weights += l.non_trainable_weights
         return weights
 
+    def get_config(self):
+        base_config = dict(super(AttentionCell, self).get_config())
+        cell_config = self._cell.get_config()
+        base_config['cell'] = {
+            'class_name': self._cell.__class__.__name__,
+            'config': cell_config
+        }
+        return base_config
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
--- src/mjlaali/problems/w_wrev.py
+++ src/mjlaali/problems/w_wrev.py
@@ -1,6 +1,9 @@
 import numpy as np
+import os, shutil
 import collections
 from tensorflow import keras
+
+from mjlaali.keras.attention import AttentionCell
 
 
 def make_dataset(l, dataset_size, num_token):
@@ -11,45 +14,85 @@ def make_dataset(l, dataset_size, num_token):
     return x, y_onehot
 
 
-def make_lstm_model(num_token, embedding_dim, lstm_units, num_layers):
+def make_lstm_model(num_token, embedding_dim, units, num_layers):
     x = keras.Input(shape=(None,), dtype='int32')
     embeddings = keras.layers.Embedding(input_dim=num_token, output_dim=embedding_dim)(x)
 
     lstm_outputs = embeddings
     for _ in range(num_layers):
         lstm_outputs = keras.layers.Bidirectional(
-            keras.layers.LSTM(units=lstm_units, return_sequences=True))(lstm_outputs)
+            keras.layers.LSTM(units=units, return_sequences=True))(lstm_outputs)
 
     y = keras.layers.Dense(units=num_token, activation='softmax')(lstm_outputs)
     return keras.models.Model(inputs=x, outputs=y)
 
 
-def try_with_bi_lstm():
+def make_attention_model(num_token, embedding_dim, units, num_layers):
+    x = keras.Input(shape=(None,), dtype='int32')
+    embeddings = keras.layers.Embedding(input_dim=num_token, output_dim=embedding_dim)(x)
+
+    lstm_outputs = embeddings
+
+    lstm_outputs = keras.layers.Bidirectional(
+        keras.layers.LSTM(units=units, return_sequences=True))(lstm_outputs)
+
+    gru_cell = keras.layers.GRUCell(units)
+    attention_cell = AttentionCell(gru_cell)
+    rnn_layer = keras.layers.RNN(attention_cell, return_sequences=True)
+
+    lstm_outputs = rnn_layer(lstm_outputs, constants=lstm_outputs)
+
+    y = keras.layers.Dense(units=num_token, activation='softmax')(lstm_outputs)
+    return keras.models.Model(inputs=x, outputs=y)
+
+
+def experiment(model_factory, name, epochs=10):
     embedding_dim = 10
-    lstm_units = 20
+    lstm_units = 10
     num_layers = 2
     num_token = 10
 
-    model = make_lstm_model(num_token=num_token, embedding_dim=embedding_dim, lstm_units=lstm_units,
-                            num_layers=num_layers)
+    model = model_factory(num_token=num_token, embedding_dim=embedding_dim, units=lstm_units, num_layers=num_layers)
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
 
     num_token = 10
     training_size = int(1e4)
-    batch_size = 100
+    batch_size = 10
 
-    for length in [2, 4, 8, 16]:
+    for length in [10, 15, 20]:
+        callbacks = [
+            keras.callbacks.EarlyStopping(patience=100),
+            keras.callbacks.ModelCheckpoint(filepath='ckpts/%s-%d/ckpt' % (name, length)),
+            keras.callbacks.TensorBoard(log_dir='ckpts/%s-%d/logs')
+        ]
+
         x, y_onehot = make_dataset(l=length, dataset_size=training_size, num_token=num_token)
         # x_valid, y_valid, y_valid_onehot = make_dataset(l=length, dataset_size=validation_size, num_token=num_token)
 
-        history = model.fit(x=x, y=y_onehot, batch_size=batch_size, validation_split=0.2, epochs=10)
+        history = model.fit(
+            x=x,
+            y=y_onehot,
+            batch_size=batch_size,
+            validation_split=0.2,
+            epochs=epochs,
+            verbose=2,
+            callbacks=callbacks
+        )
 
         x_test, y_onehot_test = make_dataset(l=length, dataset_size=4, num_token=num_token)
         y_hat = model.predict(x_test)
         print("len = %d, eval_acc=%s" % (length, history.history['val_acc'][-1]))
         print("x = \n%s\n\nexpected=\n%s\n\npredictions\n%s" % (
-        x_test, np.argmax(y_onehot_test, axis=-1), np.argmax(y_hat, axis=-1)))
+            x_test, np.argmax(y_onehot_test, axis=-1), np.argmax(y_hat, axis=-1)))
 
 
-if __name__=='__main__':
-    try_with_bi_lstm()
+if __name__ == '__main__':
+    ckpt_dir = 'ckpts'
+    if os.path.exists(ckpt_dir):
+        shutil.rmtree(ckpt_dir)
+    os.mkdir(ckpt_dir)
+
+    epochs = 1000
+    experiment(make_attention_model, name='att', epochs=epochs)
+    print("=============")
+    experiment(make_lstm_model, name='lstm', epochs=epochs)
